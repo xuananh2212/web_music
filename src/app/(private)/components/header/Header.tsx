3fe29167ece929de15b1b740bab7304abd82f6b7
xuananh <xuantuananh2212@gmail.com>
2024-09-25 21:49:57 +0700

    logo

--- src/app/(private)/components/header/Header.tsx
+++ src/app/(private)/components/header/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Input } from "antd";
+import Image from "next/image";
 import Link from "next/link";
 import styles from "./Header.module.scss";
 import ActionHeader from "./components/ActionHeader";
@@ -9,7 +10,9 @@ const Header = () => {
     <>
       <header className={styles.root}>
         <Link href="/dashboard" className="flex items-center">
-          <div className="text-neutral-500 flex gap-[6px] items-center">Logo</div>
+          <div className="text-neutral-500 flex gap-[6px] items-center">
+            <Image className="rounded-lg" quality={100} src="/logo.webp" width={35} height={35} alt="logo" />
+          </div>
         </Link>
         <Search placeholder="Tìm kiếm" style={{ width: 700 }} />
         <ActionHeader />
--- src/app/(public)/(sites)/forgot-password/page.tsx
+++
@@ -1,5 +0,0 @@
-const Page = () => {
-  return <div className="mt-32 flex items-center justify-center">forgot-password</div>;
-};
-
-export default Page;
